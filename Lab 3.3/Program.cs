d2cf9a20849936ef15a75e1c91c99790a30828e8
Роман Данилевич <dwordchanel@gmail.com>
2020-06-05 00:35:35 +0300

    Add project files.

---
+++ Lab 3.3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+namespace Lab_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText("input.txt");
+            foreach (string s in "0 1 2 3 4 5 6 7 8 9".Split(' '))
+            {
+                text = text.Replace(s, "");
+            }
+            System.IO.File.WriteAllText("output.txt", text);
+
+            Console.ReadKey();
+        }
+    }
+}
